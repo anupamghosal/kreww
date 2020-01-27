e03ff191d4fc2c3290bb0a4b45939b086d43be5f
SumanX22 <suman.mesaha22@gmail.com>
2020-01-28 02:28:16 +0530

    fixes

--- public/js/search.js
+++ public/js/search.js
@@ -1,28 +1,26 @@
 $(document).ready(()=>{
   var text = $('.mob.location-input');
-  var container = $('.mob.location-cont');
   var btn = $('.mob.cancelBtn');
   var menuItem = $(".mob.menu-item");
 
   text.focus(()=>{
-
-    container.css('width', '95vw');
     btn.css({"margin-right": "2px","opacity": "1"});
     menuItem.css("opacity", "0");
   });
 
   $('.mob.cancelBtn').click(()=>{
-    container.css('width', '80vw');
-    btn.css({"margin-right": "-5px","opacity": "0"});
-    menuItem.css("opacity", "1");
-    text.val('');
+    revert();
   });
 
   $('#hamburger').click(()=>{
-    container.css('width', '80vw');
+    revert();
+  })
+
+
+  function revert(){
     btn.css({"margin-right": "-5px","opacity": "0"});
     menuItem.css("opacity", "1");
     text.val('');
-  })
+  }
 
 });
