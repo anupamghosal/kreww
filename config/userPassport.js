b649840295b800f17b6531413b6933dd4e88e690
SumanX22 <suman.mesaha22@gmail.com>
2020-07-03 00:20:09 +0530

    addded otp fix

--- config/userPassport.js
+++ config/userPassport.js
@@ -1,41 +1,43 @@
-const LocalStrategy = require('passport-local').Strategy;
-const mongoose = require('mongoose');
-const User = require('../models/users');
-const config = require('../config/database');
-const bcrypt = require('bcryptjs');
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const User = require("../models/users");
+const config = require("../config/database");
+const bcrypt = require("bcryptjs");
 
-module.exports = function(passport) {
+module.exports = function (passport) {
   //local strategy
-  passport.use('local-user', new LocalStrategy({usernameField: 'email'}, (email,password, done)=> {
-    //matching username
-
-    User.findOne({email:email} , (err, user)=> {
-      if(err) {
-        return done(err);
-      }
-      if(!user){
-        return done(null, false, {message: 'No user found with the email address'});
-      }
-      //match the password
-      bcrypt.compare(password, user.password, (err, isMatch)=> {
-        if(err) throw err;
-        if(isMatch){
-          return done(null, user);
-        } else {
-          return done(null, false, {message: 'Wrong password'});
+  passport.use(
+    "local-user",
+    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+      User.findOne({ email: email }, (err, user) => {
+        if (err) {
+          return done(err);
         }
+        if (!user) {
+          return done(null, false, {
+            msg: "No user found with the email address",
+          });
+        }
+        //match the password
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { msg: "Wrong password" });
+          }
+        });
       });
-    });
-  }));
+    })
+  );
 
-  passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
 
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
       done(err, user);
     });
   });
-
-}
+};
--- routes/order.js
+++ routes/order.js
@@ -1,126 +1,107 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
-const passport = require('passport');
-const flash = require('connect-flash');
+const { check, validationResult } = require("express-validator");
+const passport = require("passport");
+const flash = require("connect-flash");
 
+const services = require("../Service");
 
-const services = require('../Service');
+var upcoming = require("../dateHelper");
+const appliances = require("../Appliance");
+const { ensureCustomer } = require("../config/authUser");
 
-var upcoming = require('../dateHelper');
-const appliances = require('../Appliance');
-const { ensureCustomer } = require('../config/authUser');
-
-
-
-
-const Cart = require('../models/cart');
-const User = require('../models/users');
+const Cart = require("../models/cart");
+const User = require("../models/users");
 
 //order route
-router.get('/:name', ensureCustomer, (req,res)=>{
-
-  if(appliances.some(el => el.name === req.params.name)){
-    res.render('cart',{
+router.get("/:name", ensureCustomer, (req, res) => {
+  if (appliances.some((el) => el.name === req.params.name)) {
+    res.render("cart", {
       title: req.params.name,
       appliances,
-      upcoming
+      upcoming,
     });
-
-  } else if(services.some(el => el.name === req.params.name)){
-
-    res.render('cart',{
+  } else if (services.some((el) => el.name === req.params.name)) {
+    res.render("cart", {
       title: req.params.name,
       services,
-      upcoming
+      upcoming,
     });
+  } else {
+    res.status(404).render("404");
   }
-  else{
-    res.status(404).render('404');
-  }
-
-
-  });
+});
 
 //checkout at address route
-router.post('/:name',[
-  check('address', 'Address is required').not().isEmpty(),
-  check('areacode', 'Area code is required').not().isEmpty()
-
-], (req,res)=> {
+router.post(
+  "/:name",
+  [
+    check("address", "Address is required").not().isEmpty(),
+    check("areacode", "Area code is required").not().isEmpty(),
+  ],
+  (req, res) => {
     let errors = validationResult(req);
 
-  let cart = new Cart();
-  cart.repair = req.body.repair ? true : false;
-  cart.service = req.body.service ? true : false;
+    let cart = new Cart();
+    cart.repair = req.body.repair ? true : false;
+    cart.service = req.body.service ? true : false;
 
-  if(cart.service === true || cart.repair === true){
-    cart.name = req.user.firstName+' '+req.user.lastName;
-    cart.user = req.user.id;
-    cart.address = req.body.address;
-    cart.areacode = req.body.areacode;
-    cart.work = req.params.name;
-    cart.date = req.body.date;
-    cart.time= req.body.time;
-    cart.otp = Math.floor(Math.random()*(99999 - 10000 + 1));
+    if (cart.service === true || cart.repair === true) {
+      cart.name = req.user.firstName + " " + req.user.lastName;
+      cart.user = req.user.id;
+      cart.address = req.body.address;
+      cart.areacode = req.body.areacode;
+      cart.work = req.params.name;
+      cart.date = req.body.date;
+      cart.time = req.body.time;
+      cart.otp = Math.floor(Math.random() * (99999 - 10000 + 1));
 
-
-
-    if(!errors.isEmpty()) {
-      if(appliances.some(el => el.name === req.params.name)){
-        res.render('cart',{
+      if (!errors.isEmpty()) {
+        if (appliances.some((el) => el.name === req.params.name)) {
+          res.render("cart", {
+            title: req.params.name,
+            appliances,
+            upcoming,
+            errors: errors.array(),
+          });
+        } else if (services.some((el) => el.name === req.params.name)) {
+          res.render("cart", {
+            title: req.params.name,
+            services: services,
+            upcoming,
+            month,
+            errors: errors.array(),
+          });
+        }
+      } else {
+        cart.save((err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            req.flash("success", "Order Confirmed");
+            res.render("thankyou");
+          }
+        });
+      }
+    } else {
+      req.flash("error", "Select the type of service you need");
+      if (appliances.some((el) => el.name === req.params.name)) {
+        res.render("cart", {
           title: req.params.name,
           appliances,
           upcoming,
-          errors: errors.array()
+          month,
         });
-
-      } else if(services.some(el => el.name === req.params.name)){
-
-        res.render('cart',{
+      } else if (services.some((el) => el.name === req.params.name)) {
+        res.render("cart", {
           title: req.params.name,
           services: services,
           upcoming,
           month,
-          errors: errors.array()
         });
       }
-    } else {
-      cart.save((err)=>{
-        if(err){
-          console.log(err);
-        } else {
-          req.flash('success', 'Order Confirmed')
-          res.render('thankyou')
-        }
-      });
-
-
-    }
-  }else {
-    req.flash('error', 'Select the type of service you need');
-    if(appliances.some(el => el.name === req.params.name)){
-      res.render('cart',{
-        title: req.params.name,
-        appliances,
-        upcoming,
-        month
-      });
-
-    } else if(services.some(el => el.name === req.params.name)){
-
-      res.render('cart',{
-        title: req.params.name,
-        services: services,
-        upcoming,
-        month
-      });
     }
   }
-
-
-});
-
-
+);
 
 module.exports = router;
